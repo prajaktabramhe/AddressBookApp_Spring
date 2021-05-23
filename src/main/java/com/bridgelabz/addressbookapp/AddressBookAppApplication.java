a7b1094234c4f76d3b63dbed7fd5bc4ec6a83c1c
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-23 18:37:18 +0530

    [Prajakta]: Added: setup the database and setup environment and Application Properties

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -13,6 +13,8 @@ public class AddressBookAppApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
 		log.info("Address Book app started in {} environment..", context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
+
 	}
 
 }
